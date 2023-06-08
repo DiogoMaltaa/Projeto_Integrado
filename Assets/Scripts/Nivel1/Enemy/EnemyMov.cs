b01d3910ebaa425d89817eccd66ecc5f2922e524
IvoFernandes50916 <126677172+IvoFernandes50916@users.noreply.github.com>
2023-06-08 14:53:23 +0100

    Enemy Movement
    Incomplete

---
+++ Assets/Scripts/Nivel1/Enemy/EnemyMov.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMov : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float rotationalDamp = .5f;
+    [SerializeField] float movementSpeed = 10f;
+
+    void Update()
+    {
+        Turn();
+        Move();
+    }
+    
+    void Turn()
+    {
+        Vector3 pos = target.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(pos);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationalDamp * Time.deltaTime); 
+    }
+
+    void Move()
+    {
+        transform.position += transform.forward * movementSpeed * Time.deltaTime;
+    }
+
+}
